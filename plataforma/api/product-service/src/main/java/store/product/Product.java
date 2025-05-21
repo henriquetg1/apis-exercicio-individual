c8047526a7ea9a66b5088d1723426ed817aa2424
Henrique Turco <henriqueturco1@gmail.com>
2025-05-21 19:28:35 -0300

    product-service

---
+++ plataforma/api/product-service/src/main/java/store/product/Product.java
@@ -0,0 +1,19 @@
+package store.product;
+
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Builder
+@Data @Accessors(fluent = true)
+public class Product {
+
+    private String id;
+    private String name;
+    private Double price;
+    private String unit;
+    private Date creation;
+    
+}
---
+++ plataforma/api/product-service/src/main/java/store/product/ProductRepository.java
@@ -0,0 +1,13 @@
+package store.product;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+ * https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+ */
+
+@Repository
+public interface ProductRepository extends CrudRepository<ProductModel, String> {
+    
+}
